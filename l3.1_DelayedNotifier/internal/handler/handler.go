5b6877cce69f74ed35ecd947af9bbea5dda7d8c1
Gleb <dawgkek@mail.ru>
2025-09-23 15:39:17 +0300

    added routers

--- l3.1_DelayedNotifier/internal/handler/handler.go
+++ l3.1_DelayedNotifier/internal/handler/handler.go
@@ -1,1 +1,23 @@
 package handler
+
+import "github.com/wb-go/wbf/ginext"
+
+type Handler struct{}
+
+func New() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) Router() *ginext.Engine {
+	router := ginext.New()
+
+	router.POST("/notify", h.Notify)
+	router.GET("/notify/:id", h.NotifyGetID)
+	router.DELETE("/notify/:id", h.Delete)
+
+	return router
+}
+
+func (h *Handler) Notify(c *ginext.Context)      {}
+func (h *Handler) NotifyGetID(c *ginext.Context) {}
+func (h *Handler) Delete(c *ginext.Context)      {}
