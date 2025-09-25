3bcd534e31556428481041a778934f05a1f1e5fe
Gleb <dawgkek@mail.ru>
2025-09-25 11:38:22 +0300

    added rabbitMQ - have half-part of notify

--- l3.1_DelayedNotifier/cmd/main.go
+++ l3.1_DelayedNotifier/cmd/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"time"
+
+	"github.com/wb-go/wbf/rabbitmq"
+
+	"github.com/AugustSerenity/go-contest-L3/l3.1/internal/config"
+	"github.com/AugustSerenity/go-contest-L3/l3.1/internal/handler"
+	"github.com/AugustSerenity/go-contest-L3/l3.1/internal/queue/producer"
+	"github.com/AugustSerenity/go-contest-L3/l3.1/internal/service"
+	"github.com/wb-go/wbf/zlog"
+)
+
+func main() {
+	zlog.Init()
+	zlog.Logger.Info().Msg("Starting application...")
+
+	cfgLoader := config.New()
+	if err := cfgLoader.Load("config.yaml"); err != nil {
+		zlog.Logger.Fatal().Err(err).Msg("failed to load config")
+	}
+
+	var cfg config.Config
+	if err := cfgLoader.Unmarshal(&cfg); err != nil {
+		zlog.Logger.Fatal().Err(err).Msg("failed to unmarshal config")
+	}
+
+	conn, err := rabbitmq.Connect(cfg.RabbitMQ.URL, 3, time.Second)
+	if err != nil {
+		zlog.Logger.Fatal().Err(err).Msg("failed to connect to RabbitMQ")
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		zlog.Logger.Fatal().Err(err).Msg("failed to create channel")
+	}
+	defer ch.Close()
+
+	pub := rabbitmq.NewPublisher(ch, "")
+	prod := producer.New(pub, cfg.RabbitMQ.Queue, cfg.RabbitMQ.Retry)
+
+	svc := service.New(prod)
+
+	h := handler.New(svc)
+
+	zlog.Logger.Info().Str("addr", cfg.Server.Address).Msg("starting server")
+	if err := h.Router().Run(cfg.Server.Address); err != nil {
+		zlog.Logger.Fatal().Err(err).Msg("server failed to start")
+	}
+}
--- l3.1_DelayedNotifier/internal/config/config.go
+++ l3.1_DelayedNotifier/internal/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "time"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	Server   Server         `mapstructure:"server"`
@@ -24,4 +28,22 @@ type RabbitRetryConfig struct {
 	Attempts int     `mapstructure:"attempts"`
 	DelayMS  int     `mapstructure:"delay_ms"`
 	Backoff  float64 `mapstructure:"backoff"`
+}
+
+type ConfigLoader struct {
+	v *viper.Viper
+}
+
+func New() *ConfigLoader {
+	v := viper.New()
+	return &ConfigLoader{v: v}
+}
+
+func (c *ConfigLoader) Load(path string) error {
+	c.v.SetConfigFile(path)
+	return c.v.ReadInConfig()
+}
+
+func (c *ConfigLoader) Unmarshal(rawVal any) error {
+	return c.v.Unmarshal(rawVal)
 }
